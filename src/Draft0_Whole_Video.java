bcd6f81a8e281b3919960292c031b3f76ff6e5bb
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-05-09 17:41:43 +0300

    Initial commit

--- src/Draft0_Whole_Video.java
+++ src/Draft0_Whole_Video.java
@@ -9,35 +9,62 @@ import java.net.URLConnection;
 // Видео здесь: https://lms.synergy.ru/learning/view/68493/?groupPeriodId=1045153&disciplineVersionId=25858&start=1
 
 public class Draft0_Whole_Video {
-    //        Пример 2 ППППППППППППППППППППППППППППППППППП Курс за месяц
-    public static void main(String[] args) throws IOException {
-        //        Курс за месяц: В видео мин 5.10
-        //        Если if (startIndex1 != - 1) { (если стартИндекс1 не равно -1), то тогда уже пойдёмте дальше: В видео мин 21.12
-        for (int day = 1; day <= 31; day = day + 1) {
-            String dayStr = day < 10 ? "0" + day : String.valueOf(day);
-            String url = "https://cbr.ru/scripts/XML_dynamic.asp?date_req1=01/12/2014&date_req2=" + dayStr + "/12/2014&VAL_NM_RQ=R01235";
-            String page1 = downloadWebPage(url);
-            int startIndex1 = page1.lastIndexOf("<Value>");
-            if (startIndex1 != -1) {
-                int endIndex1 = page1.lastIndexOf("</Value>");
-                String courseStr1 = page1.substring(startIndex1 + 7, endIndex1);
-                System.out.println(dayStr + "/12/2014: " + courseStr1);
-            }
-        }
-    }
+//    //        Пример 4 ППППППППППППППППППППППППППППППППППП  Фото NASA за месяц, видео мин 37.07
+//    public static void main(String[] args) {
+//
+//    } //        Конец Примера 4 КККККККККККККККК
 
-    private static String downloadWebPage(String url) throws IOException {
-        StringBuilder result = new StringBuilder();
-        String line;
-        URLConnection urlConnection = new URL(url).openConnection();
-        try (InputStream is = urlConnection.getInputStream();
-             BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
-            while ((line = br.readLine()) != null) {
-                result.append(line);
-            }
-        }
-        return result.toString();
-    } //        Конец Примера 2 КККККККККККККККК
+
+
+
+
+
+//    //        Пример 3 ППППППППППППППППППППППППППППППППППП Вложенный цикл, в видео мин 26.33
+//    public static void main(String[] args) {
+//
+//    } //        Конец Примера 3 КККККККККККККККК
+
+
+
+
+
+
+
+//    //        Пример 2 ППППППППППППППППППППППППППППППППППП Курс за месяц
+//    // Вывести курсы за все числа месяца, когда вводят месяц и год, видео мин 23.22
+//    public static void main(String[] args) throws IOException {
+//        //        Курс за месяц: В видео мин 5.10
+//        //        Если if (startIndex1 != - 1) { (если стартИндекс1 не равно -1), то тогда уже пойдёмте дальше: В видео мин 21.12
+//        for (int day = 1; day <= 31; day = day + 1) {
+//            String dayStr = day < 10 ? "0" + day : String.valueOf(day);
+//            String url = "https://cbr.ru/scripts/XML_dynamic.asp?date_req1=01/12/2014&date_req2=" + dayStr + "/12/2014&VAL_NM_RQ=R01235";
+//            String page1 = downloadWebPage(url);
+//            int startIndex1 = page1.lastIndexOf("<Value>");
+//            if (startIndex1 != -1) {
+//                int endIndex1 = page1.lastIndexOf("</Value>");
+//                String courseStr1 = page1.substring(startIndex1 + 7, endIndex1);
+//                System.out.println(dayStr + "/12/2014: " + courseStr1);
+//            }
+//        }
+//    }
+//
+//    private static String downloadWebPage(String url) throws IOException {
+//        StringBuilder result = new StringBuilder();
+//        String line;
+//        URLConnection urlConnection = new URL(url).openConnection();
+//        try (InputStream is = urlConnection.getInputStream();
+//             BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+//            while ((line = br.readLine()) != null) {
+//                result.append(line);
+//            }
+//        }
+//        return result.toString();
+//    } //        Конец Примера 2 КККККККККККККККК
+
+
+
+
+
 
 
 //    //        Пример 1 ППППППППППППППППППППППППППППППППППП
