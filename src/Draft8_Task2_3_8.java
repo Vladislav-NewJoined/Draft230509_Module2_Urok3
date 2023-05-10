b145f02f1dee383750657be88fb1a2606b347dd6
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-05-10 16:04:36 +0300

    Initial commit

---
+++ src/Draft8_Task2_3_8.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+
+//  инфо здесь: https://youtu.be/pV0v_3YRQoM
+public class Draft8_Task2_3_8 {
+//        System.out.println("Задание: \n8.\tвыведите греческий алфавит (коды с 945 по 969) через запятую (а, в, •..)\n\nРешение: ");
+
+    //        Пример 1 ППППППППППППППППППППППППППППППППППП
+    public static void main(String[] args) throws IOException {
+
+        for (int i = 945; i<=969; i++) {
+            System.out.print((char)i + ", ");
+        }
+
+
+//        System.out.println((char)1040);
+//        System.out.println((char)945);
+//        System.out.println((char)946);
+//        System.out.println((char)947);
+
+
+
+
+
+
+    }
+    //        Конец Примера 1 КККККККККККККККК
+}
